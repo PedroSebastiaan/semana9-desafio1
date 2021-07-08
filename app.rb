9e0aef44d94bab8becd0522099c94f5e4d690f0b
PedroSebastian <pizarropedrov@gmail.com>
2021-07-08 19:41:05 -0400

    fix: validations

--- app.rb
+++ app.rb
@@ -1,10 +1,42 @@
 class Appointment
     attr_reader :location, :purpose, :hour, :min
     def initialize (location, purpose, hour, min)
-        @location = location
-        @purpose = purpose
-        @hour = hour
-        @min = min
+        location_validator(location)
+        purpose_validator(purpose)
+        hour_validator(hour)
+        min_validaror(min)
+    end
+
+    def location_validator(location)
+        if location.class == String
+            @location = location
+        else
+            raise 'bad'
+        end
+    end
+
+    def purpose_validator(purpose)
+        if purpose.class == String
+            @purpose = purpose
+        else
+            raise 'bad'
+        end
+    end
+
+    def hour_validator(hour)
+        if hour.class == Integer
+            @hour = hour
+        else
+            raise 'bad'
+        end
+    end
+
+    def min_validaror(min)
+        if min.class == Integer
+            @min = min
+        else
+            raise 'bad'
+        end
     end
 end
 
@@ -12,7 +44,15 @@ class MonthlyAppointment < Appointment
     attr_reader :day
     def initialize(location, purpose, hour, min, day)
         super(location, purpose, hour, min)
-        @day = day
+        day_validator(day)
+    end
+
+    def day_validator(day)
+        if day.class == Integer
+            @day = day
+        else
+            raise 'bad'
+        end
     end
 
     def occurs_on?(day)
@@ -42,9 +82,33 @@ class OneTimeAppointment < Appointment
     attr_reader :day, :month, :year
     def initialize (location, purpose, hour, min, day, month, year)
         super(location, purpose, hour, min)
-        @day = day
-        @month = month
-        @year = year
+        day_validator(day)
+        month_validator(month)
+        year_validator(year)
+    end
+
+    def day_validator(day)
+        if day.class == Integer
+            @day = day
+        else
+            raise 'bad'
+        end
+    end
+
+    def month_validator(month)
+        if month.class == Integer
+            @month = month
+        else
+            raise 'bad'
+        end
+    end
+
+    def year_validator(year)
+        if year.class == Integer
+            @year = year
+        else
+            raise 'bad'
+        end
     end
 
     def occurs_on?(day, month, year)
